c7701d0635d7ad911e9e1f64d36cbc52a710a393
Jenny Poon <jenny.poon@live.ca>
2018-10-11 00:46:14 +0000

    refetch tweet

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,50 +1,3 @@
-// const data = [
-//   {
-//     "user": {
-//       "name": "Newton",
-//       "avatars": {
-//         "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-//         "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-//         "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-//       },
-//       "handle": "@SirIsaac"
-//     },
-//     "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//     "created_at": 1461116232227
-//   },
-//   {
-//     "user": {
-//       "name": "Descartes",
-//       "avatars": {
-//         "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-//         "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-//         "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-//       },
-//       "handle": "@rd" },
-//     "content": {
-//       "text": "Je pense , donc je suis"
-//     },
-//     "created_at": 1461113959088
-//   },
-//   {
-//     "user": {
-//       "name": "Johann von Goethe",
-//       "avatars": {
-//         "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-//         "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-//         "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-//       },
-//       "handle": "@johann49"
-//     },
-//     "content": {
-//       "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-//     },
-//     "created_at": 1461113796368
-//   }
-// ];
-
 $(function() {
 
   //==Creating New Tweets
@@ -76,6 +29,7 @@ $(function() {
 
   //==Loops through database to create tweets
   function renderTweets(tweets) {
+    $('#tweets-container').empty();
     for (let i = 0; i < tweets.length; i++) {
       let $newTweet = createTweetElement(tweets[i]);
       $('#tweets-container').append($newTweet);
@@ -96,24 +50,23 @@ $(function() {
   $('form').on('submit', (event) => {
     event.preventDefault();
 
+  $('textarea[name=text]').val();
     //Get Tweet Data
-    let data = $(event.target).serialize()
-    // console.log(data);
-    // console.log(data.length);
-    // console.log(data.value)
-    if (data.length >= 140) {
+    let data = $(event.target).serialize();
+
+    if (data.length >= 145) {
       alert("Error: Over Character Limit");
     }
     else if (data.length === 5) {
       alert("Error: No Input")
     } else {
       //Ajax Request
-      $.ajax('/tweets', {method: 'POST', data: data}).then(() => {
-        loadTweets();        // THPOILERTH !!!!
+      $.ajax('/tweets', {
+        method: 'POST',
+        data: data,
+        success: loadTweets()
+        });        // THPOILERTH !!!!
         console.log("ajax POST went through!");
-    });
-}
-
+    }
   });
 });
-
