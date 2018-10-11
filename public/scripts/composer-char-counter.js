d18b590e7e06f165a36ad1e8ff4697b4bfa3b441
Jenny Poon <jenny.poon@live.ca>
2018-10-11 04:14:27 +0000

    fixed counter so it was decreasing

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
   $("textarea").keyup(function() {
     let currentVal = 140 - this.value.length;
-    let counterElm = $(this).siblings()[1];
-    // let counterElm = $("span.counter");
+    // let counterElm = $(this).siblings()[1];
+    let counterElm = $("span.counter");
 
 //When limit is over, turn counter red
     if (currentVal >= 0) {
       $(counterElm).text(currentVal).removeClass('countertxtover');
     } else {
-      $(counterElm).text(currentVal).addClass('countertxtover');
+      $(counterElm).text(currentVal).addClass('countertxtover')
     }
   });
 });
