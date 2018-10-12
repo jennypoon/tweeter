d8e4fd4a5215c4c629383a4b5142d20232a98135
jpoon15 <40472917+jpoon15@users.noreply.github.com>
2018-10-12 15:29:53 -0700

    refactor

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,10 +1,7 @@
 $(document).ready(function() {
   $("textarea").keyup(function() {
     let currentVal = 140 - this.value.length;
-    // let counterElm = $(this).siblings()[1];
     let counterElm = $("span.counter");
-
-//When limit is over, turn counter red
     if (currentVal >= 0) {
       $(counterElm).text(currentVal).removeClass('countertxtover');
     } else {
