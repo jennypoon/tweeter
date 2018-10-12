ddcb92c12586811332db6eafdb6a3b47aa8455d8
jpoon15 <40472917+jpoon15@users.noreply.github.com>
2018-10-12 15:18:45 -0700

    Refactoring and removed unnecessary codes

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -9,11 +9,6 @@ module.exports = function makeDataHelpers(db) {
     saveTweet: function(newTweet, callback) {
       db.collection("tweets").insertOne(newTweet)
       callback(null, true);
-
-      // simulateDelay(() => {
-      //   db.tweets.push(newTweet);
-      //   callback(null, true);
-      // });
     },
 
     // Get all tweets in `db`, sorted by newest first
@@ -24,11 +19,6 @@ module.exports = function makeDataHelpers(db) {
         }
         callback(null, tweets);
       });
-
-        // simulateDelay(() => {
-        // const sortNewestFirst = (a, b) => a.created_at - b.created_at;
-        // callback(null, db.tweets.sort(sortNewestFirst));
-      // });
     }
   }
-  };
+};
