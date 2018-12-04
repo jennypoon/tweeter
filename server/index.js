01c4f0fe43985c0377981d030cb7363688712e6c
Jenny Poon <jenny.poon@live.ca>
2018-12-04 00:15:54 +0000

    fixing db connection

--- server/index.js
+++ server/index.js
@@ -1,15 +1,23 @@
+require('dotenv').config();
+
 "use strict";
 
 // Basic express setup:
 
-require('dotenv').config();
 const PORT          = process.env.PORT || 8080;
 const express       = require("express");
 const bodyParser    = require("body-parser");
 const app           = express();
 const {MongoClient} = require("mongodb");
-const MONGODB_URI   = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/tweets";
+const MONGODB_URI   = process.env.MONGODB_URI;
 
+MongoClient.connect(MONGODB_URI, (err, db) => {
+  if (err) {
+    console.error(`Failed to connect: ${MONGODB_URI}`);
+    throw err;
+  }
+
+  console.log(`Connected to mongodb: ${MONGODB_URI}`);
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
