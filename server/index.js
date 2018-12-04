61124c375dcc65dda25dc4550d6fd3bb9a40d376
jennypoon <jenny.poon@live.ca>
2018-12-03 16:18:14 -0800

    fixing db connection

--- server/index.js
+++ server/index.js
@@ -1,29 +1,17 @@
 require('dotenv').config();
-
 "use strict";
-
-// Basic express setup:
 
 const PORT          = process.env.PORT || 8080;
 const express       = require("express");
 const bodyParser    = require("body-parser");
 const app           = express();
-const {MongoClient} = require("mongodb");
-const MONGODB_URI   = process.env.MONGODB_URI;
-
-MongoClient.connect(MONGODB_URI, (err, db) => {
-  if (err) {
-    console.error(`Failed to connect: ${MONGODB_URI}`);
-    throw err;
-  }
-
-  console.log(`Connected to mongodb: ${MONGODB_URI}`);
-
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-//Mongo Connection
+const MongoClient = require("mongodb").MongoClient;
+const MONGODB_URI = process.env.MONGODB_URI;
+
 MongoClient.connect(MONGODB_URI, (err, db) => {
   if (err) {
     console.error(`Failed to connect: ${MONGODB_URI}`);
@@ -32,17 +20,13 @@ MongoClient.connect(MONGODB_URI, (err, db) => {
 
   console.log(`Connected to mongodb: ${MONGODB_URI}`);
 
-  //Allow factory functions access to Mongo db
   const DataHelpers = require("./lib/data-helpers.js")(db);
+
   const tweetsRoutes = require("./routes/tweets")(DataHelpers);
+
   app.use("/tweets", tweetsRoutes);
 
-
   app.listen(PORT, () => {
-  console.log("Example app listening on port " + PORT);
+    console.log("Example app listening on port " + PORT);
   });
-
 });
-
-
-
